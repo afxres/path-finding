3727923b3c1c017f07beeff1fcd022ab7a49a0a4
miko <stdarg@outlook.com>
2022-05-29 21:38:37 +0800

    Add basic interfaces

---
+++ code/Source/Core.Annotations/IAdjacentProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Annotations;
+
+using System.Collections.Generic;
+
+public interface IAdjacentProvider<TEntry>
+{
+    IReadOnlyCollection<TEntry> GetAdjacent(TEntry entry);
+}
---
+++ code/Source/Core.Annotations/IHeuristicValueCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Annotations;
+
+using System.Collections.Generic;
+
+public interface IHeuristicValueCalculator<TEntry, TValue> where TValue : IComparer<TValue>
+{
+    TValue Add(TValue a, TValue b);
+
+    TValue GetHeuristicValue(TEntry from, TEntry to);
+}
